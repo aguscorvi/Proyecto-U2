9343670ccbd71ff11cec1fa5f7ab7c8e21d4a2e0
aguscorvi <agustinacorvi@gmail.com>
2019-05-25 13:07:18 -0300

    u2 band

---
+++ js/main.js
@@ -0,0 +1,84 @@
+//menu//
+$('.fa-bars').click(function(){
+    $('nav').css('left', '0');
+})
+
+$('.fa-times').click(function(){
+    $('nav').css('left', '-100%');
+})
+
+$('header nav ul li a').click(function(){
+    $('nav').css('left', '-100%');
+})
+
+
+//modal1//
+$('.articulo_1 button').click(function(){
+    $('.modal1').fadeIn(300);
+    $('.modal1').css('display', 'flex');
+})
+
+$('.modal1 article button').click(function(){
+    $('.modal1').fadeOut();
+})
+
+//modal2//
+$('.articulo_2 button').click(function(){
+    $('.modal2').fadeIn(300);
+    $('.modal2').css('display', 'flex');
+})
+
+$('.modal2 article button').click(function(){
+    $('.modal2').fadeOut();
+})
+
+//modal3//
+$('.articulo_3 button').click(function(){
+    $('.modal3').fadeIn(300);
+    $('.modal3').css('display', 'flex');
+})
+
+$('.modal3 article button').click(function(){
+    $('.modal3').fadeOut();
+})
+
+//modal4//
+$('.articulo_4 button').click(function(){
+    $('.modal4').fadeIn(300);
+    $('.modal4').css('display', 'flex');
+})
+
+$('.modal4 article button').click(function(){
+    $('.modal4').fadeOut();
+})
+
+//modal5//
+$('.articulo_5 button').click(function(){
+    $('.modal5').fadeIn(300);
+    $('.modal5').css('display', 'flex');
+})
+
+$('.modal5 article button').click(function(){
+    $('.modal5').fadeOut();
+})
+
+//modal6//
+$('.articulo_6 button').click(function(){
+    $('.modal6').fadeIn(300);
+    $('.modal6').css('display', 'flex');
+})
+
+$('.modal6 article button').click(function(){
+    $('.modal6').fadeOut();
+})
+
+
+
+
+
+
+
+
+
+
+
